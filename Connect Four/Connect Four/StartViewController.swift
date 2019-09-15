c1c9c30aadced6c0299eed7e375b5bf6c8b43cfd
Christopher Vishnu <vishnuc@mcmaster.ca>
2019-09-15 02:00:08 -0400

    Renaming folder

---
+++ Connect Four/Connect Four/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  Connect Four
+//
+//  Created by Christopher Vishnu on 2019-04-23.
+//  Copyright © 2019 ChrisV. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Connect Four/Connect Four/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Connect Four
+//
+//  Created by Christopher Vishnu on 2019-04-18.
+//  Copyright © 2019 ChrisV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
